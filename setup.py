0be32e3c608490e844a64542efe0e97d3f8c8633
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-06-11 21:56:16 -0400

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hgtiles',
-    version='0.2.4',
+    version='0.2.5',
     description='Tile loading for higlass-server',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',

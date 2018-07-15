c707f4d049c1123b5352be09020bb6356c498433
Fritz Lekschas <service@lekschas.de>
2018-07-14 22:45:57 -0400

    Return internal db id of geo json annotations

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='hgtiles',
-    version='0.2.8',
+    version='0.2.9',
     description='Tile loading for higlass-server',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',

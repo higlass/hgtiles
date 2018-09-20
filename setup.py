a382ed956f15e00c06d7389b45c0f442a420e4dc
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-09-19 21:42:06 -0400

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
 
+install_requires = [
+    'cython',
+    'numpy',
+    'pysam',
+    'h5py',
+    'pandas',
+    'slugid',
+    'git+https://github.com/nvictus/pybbi.git',
+    'cooler'
+]
+
 setup(
     name='hgtiles',
     version='0.2.10',
@@ -10,5 +21,6 @@ setup(
         'pandas>=0.19',
     ],
     url='',
+    install_requires=install_requires,
     packages=['hgtiles'],
 )

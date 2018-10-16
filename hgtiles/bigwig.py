481700cae09151486b3407b3e7045346fb74450e
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-10-16 11:21:49 -0400

    Int-ify chromsizes

--- hgtiles/bigwig.py
+++ hgtiles/bigwig.py
@@ -116,10 +116,11 @@ def tileset_info(bwpath, chromsizes=None):
         chromsizes_list = []
 
         for chrom,size in chromsizes.iteritems():
-            chromsizes_list += [[chrom, size]]
+            chromsizes_list += [[chrom, int(size)]]
     else:
         chromsizes_list = chromsizes
     
+    print("hi")
     min_tile_cover = np.ceil(sum([int(c[1]) for c in chromsizes_list]) / TILE_SIZE)
     max_zoom = int(np.ceil(np.log2(min_tile_cover)))
 
